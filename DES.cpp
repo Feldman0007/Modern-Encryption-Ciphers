f0f69d7f7272b0e8551516e26773f8d63de75d7f
Alexander Feldman <35717802+Feldman0007@users.noreply.github.com>
2020-03-22 15:01:22 -0700

    Add files via upload

---
+++ DES.cpp
@@ -0,0 +1,212 @@
+#include "DES.h"
+#define ENC 1
+#define DEC 0
+/**
+ * Sets the key to use
+ * @param key - the key to use
+ * @return - True if the key is valid and False otherwise
+ */
+bool DES::setKey(const unsigned char* keyArray)
+{
+	/**
+	 * First let's covert the char string
+	 * into an integer byte string
+	 */
+	
+	
+	/* The key error code */
+	int keyErrorCode = -1;
+
+	/* A single byte */
+	unsigned char singleByte = 0;	
+	
+	/* The key index */
+	int keyIndex = 0;
+	
+	/* The DES key index */
+	int desKeyIndex = 0;
+		
+	/* Go through the entire key character by character */
+	while(desKeyIndex != 8)
+	{
+		/* Convert the key if the character is valid */
+		if((this->des_key[desKeyIndex] = twoCharToHexByte(keyArray + keyIndex)) == 'z')
+			return false;
+		
+		/* Go to the second pair of characters */
+		keyIndex += 2;	
+		
+		/* Increment the index */
+		++desKeyIndex;
+	}
+	
+	fprintf(stdout, "DES KEY: ");
+	
+	/* Print the key */
+	for(keyIndex = 0; keyIndex < 8; ++keyIndex)
+		fprintf(stdout, "%x", this->des_key[keyIndex]);
+	
+	fprintf(stdout, "\n");	
+	
+	
+	/* Set the encryption key */
+	if ((keyErrorCode = des_set_key_checked(&des_key, this->key)) != 0)
+	{
+		fprintf(stderr, "\nkey error %d\n", keyErrorCode);
+		
+		return false;
+	}
+	
+	/* All is well */	
+	return true;
+}
+
+/**	
+ * Encrypts a plaintext block 8 characters at a time
+ * @param plaintext - the plaintext string
+ * @return - the encrypted ciphertext string
+ */
+unsigned char* DES::encrypt(const unsigned char* plaintext)
+{
+	unsigned char cipherText[9]; //ciphertext block
+	memset(cipherText, 0, 9);
+
+	DES_LONG block[2];
+	block[0] = ctol(plaintext);
+	block[1] = ctol(plaintext + 4);
+	
+	des_encrypt1(block, this->key, ENC);
+	
+
+	ltoc(block[0], cipherText);
+	ltoc(block[1], cipherText + 4);
+
+	//store the block of ciphertext in a buffer that will be written to the file
+	unsigned char * ciphertextBlockPtr = cipherText;
+
+	return ciphertextBlockPtr;
+}
+
+/**
+ * Decrypts a string of ciphertext
+ * @param ciphertext - the ciphertext
+ * @return - the plaintext
+ */
+unsigned char* DES::decrypt(const unsigned char* ciphertext)
+{
+	//LOGIC:
+	// Same logic as encrypt(), except in step 4. decrypt instead of encrypting
+	unsigned char plainText[9]; //ciphertext block
+	memset(plainText, 0, 9);
+
+	DES_LONG block[2];
+	block[0] = ctol(ciphertext);
+	block[1] = ctol(ciphertext + 4);
+
+	des_encrypt1(block, this->key, DEC);
+
+
+	ltoc(block[0], plainText);
+	ltoc(block[1], plainText + 4);
+
+	//store the block of ciphertext in a buffer that will be written to the file
+	unsigned char * ciphertextBlockPtr = plainText;
+
+	return ciphertextBlockPtr;
+}
+
+/**
+ * Converts an array of 8 characters
+ * (i.e. 4 bytes/32 bits)
+ * @param c - the array of 4 characters (i.e. 1-byte per/character
+ * @return - the long integer (32 bits) where each byte
+ * is equivalent to one of the bytes in a character array
+ */
+DES_LONG DES::ctol(unsigned char *c) 
+{
+        /* The long integer */
+	DES_LONG l;
+        
+	l =((DES_LONG)(*((c)++)));
+        l = l | (((DES_LONG)(*((c)++)))<<8L);
+        l = l | (((DES_LONG)(*((c)++)))<<16L);
+        l = l | (((DES_LONG)(*((c)++)))<<24L);
+        return l;
+};
+
+
+/** 
+ * Converts a long integer (4 bytes = 32 bits)
+ * into an array of 8 characters.
+ * @param l - the long integer to convert
+ * @param c - the character array to store the result
+ */
+void DES::ltoc(DES_LONG l, unsigned char *c) 
+{
+        *((c)++)=(unsigned char)(l&0xff);
+        *((c)++)=(unsigned char)(((l)>> 8L)&0xff);
+        *((c)++)=(unsigned char)(((l)>>16L)&0xff);
+        *((c)++)=(unsigned char)(((l)>>24L)&0xff);
+}
+
+/**
+ * Converts a character into a hexidecimal integer
+ * @param character - the character to convert
+ * @return - the converted character, or 'z' on error
+ */
+unsigned char DES::charToHex(const char& character)
+{
+	/* Is the first digit 0-9 ? */	
+	if(character >= '0' && character <= '9')	
+		/* Convert the character to hex */
+		return character - '0';
+	/* It the first digit a letter 'a' - 'f'? */
+	else if(character >= 'a' && character <= 'f')
+		/* Conver the cgaracter to hex */
+		return (character - 97) + 10;	
+	/* Invalid character */
+	else return 'z';
+}
+
+/**
+ * Converts two characters into a hex integers
+ * and then inserts the integers into the higher
+ * and lower bits of the byte
+ * @param twoChars - two charcters representing the
+ * the hexidecimal nibbles of the byte.
+ * @param twoChars - the two characters
+ * @return - the byte containing having the
+ * valud of two characters e.g. string "ab"
+ * becomes hexidecimal integer 0xab.
+ */
+unsigned char DES::twoCharToHexByte(const unsigned char* twoChars)
+{
+	/* The byte */
+	unsigned char singleByte;
+	
+	/* The second character */
+	unsigned char secondChar;
+
+	/* Convert the first character */
+	if((singleByte = charToHex(twoChars[0])) == 'z') 
+	{
+		/* Invalid digit */
+		return 'z';
+	}
+	
+	/* Move the newly inserted nibble from the
+	 * lower to upper nibble.
+	 */
+	singleByte = (singleByte << 4);
+	
+	/* Conver the second character */
+	if((secondChar = charToHex(twoChars[1])) == 'z')
+		return 'z'; 
+	
+	/* Insert the second value into the lower nibble */	
+	singleByte |= secondChar;
+
+	return singleByte;
+}
+
+
---
+++ cipher.cpp
@@ -0,0 +1,276 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "CipherInterface.h"
+#include "AES.h"
+#include "DES.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*Case insensitive string comparison helper function used in processing command line arguments*/
+bool iequals(const string&, const string&);
+/*Error checking to see if we have correctly set up an instance of our cipher*/
+void assertValidCipherAssignment(const CipherInterface*);
+/*Validate the key against the type of cypher selected and assign the cipher's key*/
+void validateAndSetKey(CipherInterface* const, const string&);
+/*Perform either encryption or decryption and write to output file*/
+void performOperation(CipherInterface* const, const string&, const string&, const string&, const bool&);
+
+int main(int argc, char** argv)
+{
+
+	////Testing
+	//CipherInterface* cipher = new DES();
+	//CipherInterface* cipher1 = new AES();
+
+	//validateAndSetKey(cipher, "0123456789abcdef");
+	//validateAndSetKey(cipher1, "00112233445566778899aabbccddeeff");
+
+	//performOperation(cipher, "enc", "small.txt", "desencrypt.txt");
+	//performOperation(cipher1, "enc", "small.txt", "aesencrypt.txt");;
+
+	//performOperation(cipher, "enc", "big.txt", "desencrypt.txt");
+	//performOperation(cipher1, "enc", "big.txt", "aesencrypt.txt");
+
+	//performOperation(cipher, "dec", "desencrypt.txt", "desdecrypt.txt");
+	//performOperation(cipher1, "dec", "csencrypt.txt", "aesdecrypt.txt");
+
+	//return 0;
+
+	/*Make sure we have only 5 command line arguments before moving forward*/
+	if (argc != 6)
+	{
+		cout << "cipher.exe only accepts 5 arguments: <CIPHER NAME> <KEY> <ENC/DEC> <INPUTFILE> <OUTPUT FILE>" << endl;
+		exit(-1);
+	}
+
+	/*Variables used to parse the command line argument and execute the ciphers dynamically*/
+	string cipherName = argv[1];
+	string key = argv[2];
+	string operation = argv[3];
+	string inputFileName = argv[4];
+	string outputFileName = argv[5];
+
+	CipherInterface* cipher = NULL; /*pointer to an instance of our cipher*/
+	bool isAES = false; //false = DES, true = AES
+
+	if (iequals(cipherName, "AES"))
+	{
+		cipher = new AES();
+		isAES = true;
+		if (iequals(operation, "ENC"))
+		{
+			key.insert(0, 1, '1');
+		}
+		else
+		{
+			key.insert(0, 1, '0');
+		}
+	}
+	else if (iequals(cipherName, "DES"))
+	{
+		cipher = new DES();
+	}
+	else
+	{
+		assertValidCipherAssignment(cipher);
+	}
+
+	validateAndSetKey(cipher, key);
+	performOperation(cipher, operation, inputFileName, outputFileName, isAES);
+
+	return 0;
+}
+
+void validateAndSetKey(CipherInterface* const cipher, const string& key)
+{
+	bool validKey = cipher->setKey((const unsigned char *)key.c_str());
+	if (!validKey)
+	{
+		cout << "This key is not valid for the selected cipher!" << endl;
+		exit(-1);
+	}
+}
+
+void performOperation(CipherInterface* const cipher, const string& operation, const string& inputFilename, const string& outputFilename, const bool& isAES)
+{
+	ifstream fileReader;
+	fileReader.open(inputFilename);
+	if (fileReader.fail()) {
+		cout << "Error opening input file!" << endl;
+		exit(-1);
+	}
+	if (fileReader.peek() == std::ifstream::traits_type::eof())
+	{
+		cout << "Input file has no text!";
+		exit(-1);
+	}
+	
+	ofstream fileWriter;
+	fileWriter.open(outputFilename);
+	if (fileWriter.fail()) {
+		cout << "Error opening output file!" << endl;
+		exit(-1);
+	}
+	if (fileReader.peek() == std::ifstream::traits_type::eof())
+	{
+		cout << "Input file has no text!";
+		exit(-1);
+	}
+
+	/*Encrypt or decrypt block by block*/
+	if (iequals(operation, "ENC"))
+	{
+		//Depending on the algorithm used we will be encrypting/decrypting using different block sizes
+		if (isAES) 
+		{
+			bool doneEncrypting = false;
+
+			while (!doneEncrypting) 
+			{
+				unsigned char plaintextBlock[17]; //block of plaintext to encrypt
+				unsigned char * plaintextBlockPtr = plaintextBlock; //used to pass block to encrypt function
+				for (int i = 0; i < 16; i++)
+				{
+					if (!fileReader.eof())
+					{
+						plaintextBlock[i] = (unsigned char)fileReader.get();
+					}
+					else
+					{
+						plaintextBlock[i] = '0'; //If we have read the whole file but the block is not filled, start padding
+						doneEncrypting = true; //we have reached the end of the file so we are done encrypting after this run
+					}
+				}
+				plaintextBlock[16] = '\0';
+				unsigned char * ciphertextBlockPtr = cipher->encrypt(plaintextBlockPtr); //encrypt block
+				for (int i = 0; i < 16; i++)
+				{
+					fileWriter << ciphertextBlockPtr[i]; //write encrypted block to file
+				}
+			}
+		}
+		else //DES
+		{
+			bool doneEncrypting = false;
+
+			while (!doneEncrypting)
+			{
+				unsigned char plaintextBlock[9];
+				unsigned char * plaintextBlockPtr = plaintextBlock;
+				for (int i = 0; i < 8; i++)
+				{
+					if (!fileReader.eof())
+					{
+						plaintextBlock[i] = (unsigned char)fileReader.get();
+					}
+					else
+					{
+						plaintextBlock[i] = '0';
+						doneEncrypting = true;
+					}
+				}
+				plaintextBlock[8] = '\0';
+				unsigned char * ciphertextBlockPtr = cipher->encrypt(plaintextBlockPtr);
+				for (int i = 0; i < 8; i++)
+				{
+					fileWriter << ciphertextBlockPtr[i];
+				}
+			}
+		}
+		fileReader.close();
+		fileWriter.close();
+	}
+	else if (iequals(operation, "DEC"))
+	{
+		if (isAES)
+		{
+			bool doneDecrypting = false;
+
+			while (!doneDecrypting)
+			{
+				unsigned char ciphertextBlock[17];
+				unsigned char * ciphertextBlockPtr = ciphertextBlock;
+				for (int i = 0; i < 16; i++)
+				{
+					if (!fileReader.eof())
+					{
+						ciphertextBlock[i] = (unsigned char)fileReader.get();
+					}
+					else
+					{
+						ciphertextBlock[i] = '0';
+						doneDecrypting = true;
+					}
+				}
+				ciphertextBlock[16] = '\0';
+				unsigned char * plaintextBlockPtr = cipher->encrypt(ciphertextBlockPtr);
+				for (int i = 0; i < 16; i++)
+				{
+					fileWriter << plaintextBlockPtr[i];
+				}
+			}
+		}
+		else //DES
+		{
+			bool doneDecrypting = false;
+
+			while (!doneDecrypting)
+			{
+				unsigned char ciphertextBlock[9];
+				unsigned char * ciphertextBlockPtr = ciphertextBlock;
+				for (int i = 0; i < 8; i++)
+				{
+					if (!fileReader.eof())
+					{
+						ciphertextBlock[i] = (unsigned char)fileReader.get();
+					}
+					else
+					{
+						ciphertextBlock[i] = '0';
+						doneDecrypting = true;
+					}
+				}
+				ciphertextBlock[8] = '\0';
+				unsigned char * plaintextBlockPtr = cipher->encrypt(ciphertextBlockPtr);
+				for (int i = 0; i < 8; i++)
+				{
+					fileWriter << plaintextBlockPtr[i];
+				}
+			}
+		}
+		fileReader.close();
+		fileWriter.close();
+	}
+	else
+	{
+		fileReader.close();
+		fileWriter.close();
+		cout << "This is not a valid operation!" << endl;
+		exit(-1);
+	}
+}
+
+bool iequals(const string& a, const string& b) //case insensitive comparison
+{
+	unsigned int sz = a.size();
+	if (b.size() != sz)
+		return false;
+	for (unsigned int i = 0; i < sz; ++i)
+		if (tolower(a[i]) != tolower(b[i]))
+			return false;
+	return true;
+}
+
+void assertValidCipherAssignment(const CipherInterface* cipher)
+{
+	/*Error checking*/
+	if (!cipher)
+	{
+		fprintf(stderr, "ERROR [%s %s %d]: could not allocate memory\n",
+			__FILE__, __FUNCTION__, __LINE__);
+		exit(-1);
+	}
+}
